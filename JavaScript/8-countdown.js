d1ba2ea9b06f3af8f2507009c9a72280cc2a1bf9
lokesh2108 <65254815+lokesh2108@users.noreply.github.com>
2022-10-09 21:12:12 -0700

    done

--- JavaScript/8-countdown.js
+++ JavaScript/8-countdown.js
@@ -1,4 +1,4 @@
-var Start = parseInt(window.prompt("Enter the Start Number"));
-for(var i=Start;i>=0;i--) {
+var start = parseInt(window.prompt("Enter the Start Number"));
+for(var i=start;i>=0;i--) {
     window.document.write(i + "<br>");
 }
